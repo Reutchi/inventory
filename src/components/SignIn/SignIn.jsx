0b766a17472558b2ace688ccbfae706231e907a9
Developer <developer@MacBook-Pro-Dan.local>
2023-12-12 09:42:59 +0200

    add redux thunk logic

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,34 +1,34 @@
 import './sign-in.scss'
-import {BaseInput, Button} from "../index";
-import {useDispatch,useSelector} from "react-redux";
-import {SIGN_IN, userLogin} from "../../store/modules/auth";
-import { useNavigate } from 'react-router-dom';
+import {BaseInput, Button} from "../index"
+import {useDispatch,useSelector} from "react-redux"
+import { useNavigate } from 'react-router-dom'
 const SignIn = ({title = 'Bine ai revenit'}) => {
+
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const {signInForm,token} = useSelector((state) => state.auth)
 
-    const handleSignIn = ({key,value}) => {
-        dispatch(SIGN_IN({key, value}))
-    }
+    // const handleSignIn = ({key,value}) => {
+    //     dispatch(SIGN_IN({key, value}))
+    // }
 
-
-    const handleSubmit = (ev) => {
-        ev.preventDefault()
-        dispatch(userLogin(signInForm,navigate))
-    }
-
-    localStorage.setItem('token',token)
-
-
-
+    // const handleSubmit = async (ev) => {
+    //     ev.preventDefault();
+    //     try {
+    //         const response = await dispatch(userLogin(data));
+    //         if (response && response.status === 200) {
+    //             navigate('/')
+    //         }
+    //     } catch (err) {
+    //         console.log(err)
+    //     }
+    // };
 
     return (
         <div className="form sign-in">
             <h2>{title}</h2>
-            <BaseInput required name='email' type='email' onInput={handleSignIn} label='Email'/>
-            <BaseInput required name='password' type='password' onInput={handleSignIn} label='Password'/>
-            <Button onClick={handleSubmit} type='submit' title="Autentificare"/>
+            <BaseInput required nickname='email' type='email'  label='Email'/>
+            <BaseInput required nickname='password' type='password'  label='Password'/>
+            <Button type='submit' title="Autentificare"/>
         </div>
     )
 }
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,61 +1,59 @@
-import './sign-up.scss'
-import {BaseAvatar, BaseInput, Button} from "../index";
-import React, {useCallback} from "react";
-import {HANDLE_FORM, userRegister} from "../../store/modules/auth";
-import {useDispatch, useSelector} from "react-redux";
+import React, {useEffect} from 'react';
+import { useForm } from 'react-hook-form';
+import './sign-up.scss';
+import { Button } from '../index';
+import { registerUser } from '../../store/modules/auth';
+import { useDispatch, useSelector } from 'react-redux';
 import {useNavigate} from "react-router-dom";
 
-const SignUp = ({title = 'E timpul să te simți ca acasă'}) => {
+const SignUp = ({ title = 'E timpul să te simți ca acasă' }) => {
+    const dispatch = useDispatch();
+    const {authenticated} = useSelector((state) => state.auth);
+    const {register, handleSubmit, watch, formState: { errors },} = useForm()
+
     const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const {  registerForm, avatarUrl , token} = useSelector((state) => state.auth);
 
-    localStorage.setItem('token',token)
+    useEffect(() => {
+        authenticated ? navigate('/') : navigate('/auth')
+    }, [])
 
-    const handleInput = useCallback(({ key, value }) => {
-        dispatch(HANDLE_FORM({ key, value }));
-    }, [dispatch]);
-
-    const handleSubmit = useCallback((ev) => {
-        ev.preventDefault();
-
-        const formData = new FormData();
-
-        Object.entries(registerForm).forEach(([key, value]) => {
-            formData.append(key, value);
-        });
-
-        formData.append('avatar', avatarUrl);
-
-        dispatch(userRegister(formData,navigate));
-    }, [dispatch, registerForm, avatarUrl]);
-
-
-    const inputsType = [
-        {id:1,label:'Name',type:'text',name:'name'},
-        {id:2,label:'Email',type:'email',name:'email'},
-        {id:3,label:'Password',type:'password',name:'password'},
-    ]
-
+    const submitForm = (data) => {
+        dispatch(registerUser(data))
+    }
 
     return (
-        <form onSubmit={ev => handleSubmit(ev)} className="form sign-up">
+        <form onSubmit={handleSubmit(submitForm)} className="form sign-up">
             <h2>{title}</h2>
-            <BaseAvatar/>
-            {inputsType.map(({label,type,name,id}) => {
-                return(
-                    <BaseInput
-                        key={id}
-                        onInput={handleInput}
-                        label={label}
-                        type={type}
-                        required
-                        name={name}
-                    />
-                )
-            })}
+            <label htmlFor='nickname'>
+                <span>NAME</span>
+                <input
+                    type='text'
+                    className='form-input'
+                    {...register('nickname')}
+                    required
+                />
+            </label>
+            <label htmlFor='email'>
+                <span>Email</span>
+                <input
+                    type='email'
+                    className='form-input'
+                    {...register('email')}
+                    required
+                />
+            </label>
+            <label htmlFor='password'>
+                <span>Password</span>
+                <input
+                    type='password'
+                    className='form-input'
+                    {...register('password')}
+                    required
+                />
+            </label>
             <Button type='submit' title='Creaza Cont'/>
         </form>
-    )
-}
-export default SignUp
+    );
+};
+
+export default SignUp;
